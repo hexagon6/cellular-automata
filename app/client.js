db8970e90f7195bee50f3cce383f3634e9a0e225
Tobi Turing <dev@fet.li>
2018-04-29 19:18:33 +0200

    feature(sapper): add svelte store

--- app/client.js
+++ app/client.js
@@ -1,7 +1,14 @@
 import { init } from 'sapper/runtime.js'
+import { Store } from 'svelte/store.js'
+
 import { routes } from './manifest/client.js'
 
 // `routes` is an array of route objects injected by Sapper
-init(document.querySelector('#sapper'), routes)
+init(document.querySelector('#sapper'), routes, {
+  store: data => {
+    // `data` is whatever was in the server-side store
+    return new Store(data)
+  },
+})
 
 if (module.hot) module.hot.accept()
--- app/server.js
+++ app/server.js
@@ -2,8 +2,21 @@ import polka from 'polka'
 import compression from 'compression'
 import sapper from 'sapper'
 import serve from 'serve-static'
+import { Store } from 'svelte/store.js'
 import { routes } from './manifest/server.js'
 
 polka() // You can also use Express
-  .use(compression({ threshold: 0 }), serve('assets'), sapper({ routes }))
+  .use(
+    compression({ threshold: 0 }),
+    serve('assets'),
+    // authenticationMiddleware(), // TODO: implement user session handling
+    sapper({
+      routes,
+      store: request => {
+        return new Store({
+          user: request.user,
+        })
+      },
+    })
+  )
   .listen(process.env.PORT)
