90b06134a6bcc1ca37540473551bef9914d01cb8
Tobi Turing <dev@fet.li>
2018-05-13 13:45:14 +0200

    cleanup

--- modules/stores/gameoflife.js
+++ modules/stores/gameoflife.js
@@ -1,13 +1,13 @@
 const fieldtypes = ['hexagonal', 'rectangular']
 const stati = [
   ['#ff6738', '#18c8ff', '#efaf03', '#60f370'],
-  ['#ffffff', '#efefef']
+  ['#ffffff', '#efefef'],
 ]
 
 export default {
   name: 'Game of Life',
   algorithm: {
-    name: 'GoL'
+    name: 'GoL',
   },
   field: [],
   fieldtype: fieldtypes[0],
