9d7edfb6fd0c333ef20617ae35a2165b97e522d8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-14 14:59:07 +0000

    Deploy hexagon6/sapper-cellular-automata to github.com/hexagon6/sapper-cellular-automata.git:gh-pages

--- modules/index.js
+++
@@ -1,8 +0,0 @@
-import hexagonal from './shapes/hexagon'
-import rectangular from './shapes/rectangular'
-
-import { GameofLifeDefault, WavesDefault } from './stores'
-
-const algorithm = () => 0
-
-export { algorithm, hexagonal, rectangular, GameofLifeDefault, WavesDefault }
