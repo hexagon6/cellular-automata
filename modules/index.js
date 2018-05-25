f88ba5d9290038ea5f541d8a3dc878284cecd8aa
Tobi Turing <dev@fet.li>
2018-05-25 19:49:59 +0300

    refactor(Components):  move field generation into route page & implement naive algorithm

--- modules/index.js
+++ modules/index.js
@@ -5,4 +5,25 @@ import { GameofLifeDefault, WavesDefault } from './stores'
 
 const algorithm = () => 0
 
-export { algorithm, hexagonal, rectangular, GameofLifeDefault, WavesDefault }
+const fieldGen = type => {
+  const _default = 'rectangular'
+  const generators = {
+    hexagonal: hexagonal.field,
+    rectangular: rectangular.field,
+  }
+  return type in generators ? generators[type] : generators[_default]
+}
+
+const initializer = (n, i) => n % i
+const randomInitializer = (n, i) => Math.floor(Math.random() * n) % i
+
+export {
+  algorithm,
+  hexagonal,
+  rectangular,
+  GameofLifeDefault,
+  WavesDefault,
+  fieldGen,
+  initializer,
+  randomInitializer,
+}
