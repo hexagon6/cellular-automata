c040199416ab3bb213992954ddd95e0e0a59d84d
Tobi Turing <dev@fet.li>
2018-05-28 02:16:18 +0300

    tests(golHex): up coverage with golHex test

--- tests/algorithm.test.js
+++ tests/algorithm.test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import { naive, naiveNB, countCells } from '../modules/'
-import { gol } from '../modules/algorithms/gameoflife'
+import { gol, golHex } from '../modules/algorithms/gameoflife'
 
 const cellZero = { x: 0, y: 0, v: 0 }
 const cellOne = { x: 0, y: 0, v: 1 }
@@ -57,6 +57,32 @@ test('Game of Life', t => {
   }
   t.deepEqual(gol(6, 6)(cellsBefore), cellsAfter)
   t.deepEqual(gol(6, 6)(cellsAfter), cellsBefore)
+})
+
+test('hexagonal Game of Life', t => {
+  const cellsBefore = {
+    '0x0': 0,
+    '0x1': 1,
+    '0x2': 1,
+    '1x0': 0,
+    '1x1': 0,
+    '1x2': 0,
+    '2x0': 0,
+    '2x1': 0,
+    '2x2': 0,
+  }
+  const cellsAfter = {
+    '0x0': 0,
+    '0x1': 0,
+    '0x2': 0,
+    '1x0': 0,
+    '1x1': 0,
+    '1x2': 0,
+    '2x0': 0,
+    '2x1': 0,
+    '2x2': 0,
+  }
+  t.deepEqual(golHex(6, 6)(cellsBefore), cellsAfter)
 })
 
 test.todo('calculate new state based on neighbors')
