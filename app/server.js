8fa598c3a238a4bf504e2ccef6ab82bda2506bdc
Tobi Turing <dev@fet.li>
2018-10-08 19:00:24 +0200

    chore(npm): upgrade to sapper 0.15 & migrate App page to _layout

--- app/server.js
+++ app/server.js
@@ -3,16 +3,14 @@ import compression from 'compression'
 import sapper from 'sapper'
 import serve from 'serve-static'
 import { Store } from 'svelte/store.js'
-import { routes } from './manifest/server.js'
-import App from './App.html'
+import { manifest } from './manifest/server.js'
 
 const serverArgs = [
   compression({ threshold: 0 }),
   serve('assets'),
   // authenticationMiddleware(), // TODO: implement user session handling
   sapper({
-    routes,
-    App,
+    manifest,
     store: request => {
       return new Store({
         user: request.user,
