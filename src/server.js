e3c52805af8bd6509890a900b528f35a5402033d
Tobi Turing <dev@fet.li>
2018-10-08 20:52:18 +0200

    chore(npm): upgrade to sapper 0.22: migrate

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 import polka from 'polka'
 import compression from 'compression'
-import sapper from 'sapper'
 import serve from 'serve-static'
 import { Store } from 'svelte/store.js'
-import { manifest } from './manifest/server.js'
+import * as sapper from '../__sapper__/server.js'
 
 const serverArgs = [
   compression({ threshold: 0 }),
   serve('static'),
   // authenticationMiddleware(), // TODO: implement user session handling
-  sapper({
-    manifest,
+  sapper.middleware({
     store: request => {
       return new Store({
         user: request.user,
